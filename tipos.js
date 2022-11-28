55bb2ff4ef2db09e373868e4da4848a5997ff8eb
Salvador Carias <scarias@gbm.net>
2022-11-28 13:00:42 -0600

    fin tipos

---
+++ tipos.js
@@ -0,0 +1,18 @@
+// Tipos de Datos en JavaScript
+
+// Primitivos
+
+var numeros = 4
+var String = "Hola"
+var booleanos = true
+var nulos = null
+var noDefinidos = undefined
+
+// Objetos
+
+var userInfo = {
+    "Nombre": "Benjamin",
+    "Edad": 2
+}
+
+console.log(userInfo)
