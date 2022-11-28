0697115acedfa59651058a5b5d79686e22cc27cf
Salvador Carias <scarias@gbm.net>
2022-11-28 13:07:13 -0600

    fin variables

--- variables.js
+++ variables.js
@@ -1,8 +1,10 @@
 // Variables en Javascript
 
+// Var puede ser accedida desde cualquier parte del codigo
 var nombreUser = "Benjamin"
+
+// Let solo puede ser accedido desde el bloque de codigo donde se creo
 let edadUser = 21
 
 // Constante: estas su valor no puede cambiar
-
 const codigoIdentidad = "CEFERF23234"
